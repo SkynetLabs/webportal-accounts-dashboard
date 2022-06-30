8e79834b05b0d8f8a915feea664fc71a4240532d
Michał Leszczyk <michasko37@gmail.com>
2022-06-30 10:38:46 +0200

    Stub API requests

--- cypress/e2e/login-page.test.js
+++ cypress/e2e/login-page.test.js
@@ -1,9 +1,27 @@
-context("/", () => {
-  beforeEach(() => {
-    cy.visit("");
-  });
+/// <reference types="cypress" />
 
-  it("should render page title", () => {
-    cy.title().should("eq", "Sign In | Account Dashboard");
+context("/", () => {
+  context("for guest user", () => {
+    beforeEach(() => {
+      cy.intercept("GET", "/api/user", (req) => {
+        req.reply({
+          statusCode: 401,
+          body: { message: "no api key found" },
+        });
+      });
+      cy.intercept("/__internal/do/not/use/accounts", {
+        subscription_required: false,
+        subscription: false,
+        authenticated: true,
+        enabled: true,
+        auth_required: true,
+      });
+
+      cy.visit("");
+    });
+
+    it("should render page title", () => {
+      cy.title().should("eq", "Sign In | Account Dashboard");
+    });
   });
 });
